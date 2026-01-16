9c35e869c436af6105d0671eac38832f44f4cf1a
Stanley Ponraj <stanleyponraj@Stanleys-MacBook-Air.local>
2026-01-16 23:32:56 +0530

    Fix startup: ensure migrations + disable email logging

--- horilla/settings.py
+++ horilla/settings.py
@@ -246,3 +246,8 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+}
+
